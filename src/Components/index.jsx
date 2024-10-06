8e09a22162f7d128396f3c19644809d6c370947f
bazil-1854 <bscs22072@itu.edu.pk>
2024-10-07 00:49:40 +0500

    Add "Dashboard" of Admin

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -1,221 +1,9 @@
-import React from 'react';
-import "chart";
-import 'jsvectormap'; // Import the jsVectorMap library
-import 'jsvectormap/dist/maps/world.js'; // Import the specific map you want to use
+import React, { useEffect } from 'react'; 
+import "../Static/scss/app.scss"
 
 const Index = () => {
-    document.addEventListener("DOMContentLoaded", function () {
-        var date = new Date(Date.now() - 5 * 24 * 60 * 60 * 1000);
-        var defaultDate = date.getUTCFullYear() + "-" + (date.getUTCMonth() + 1) + "-" + date.getUTCDate();
-        document.getElementById("datetimepicker-dashboard").flatpickr({
-            inline: true,
-            prevArrow: "<span title=\"Previous month\">&laquo;</span>",
-            nextArrow: "<span title=\"Next month\">&raquo;</span>",
-            defaultDate: defaultDate
-        });
-    });
-    document.addEventListener("DOMContentLoaded", function () {
-        var markers = [{
-            coords: [31.230391, 121.473701],
-            name: "Shanghai"
-        },
-        {
-            coords: [28.704060, 77.102493],
-            name: "Delhi"
-        },
-        {
-            coords: [6.524379, 3.379206],
-            name: "Lagos"
-        },
-        {
-            coords: [35.689487, 139.691711],
-            name: "Tokyo"
-        },
-        {
-            coords: [23.129110, 113.264381],
-            name: "Guangzhou"
-        },
-        {
-            coords: [40.7127837, -74.0059413],
-            name: "New York"
-        },
-        {
-            coords: [34.052235, -118.243683],
-            name: "Los Angeles"
-        },
-        {
-            coords: [41.878113, -87.629799],
-            name: "Chicago"
-        },
-        {
-            coords: [51.507351, -0.127758],
-            name: "London"
-        },
-        {
-            coords: [40.416775, -3.703790],
-            name: "Madrid "
-        }
-        ];
-        var map = new jsVectorMap({
-            map: "world",
-            selector: "#world_map",
-            zoomButtons: true,
-            markers: markers,
-            markerStyle: {
-                initial: {
-                    r: 9,
-                    strokeWidth: 7,
-                    stokeOpacity: .4,
-                    fill: window.theme.primary
-                },
-                hover: {
-                    fill: window.theme.primary,
-                    stroke: window.theme.primary
-                }
-            },
-            zoomOnScroll: false
-        });
-        window.addEventListener("resize", () => {
-            map.updateSize();
-        });
-    });
-    document.addEventListener("DOMContentLoaded", function () {
-        var ctx = document.getElementById("chartjs-dashboard-line").getContext("2d");
-        var gradient = ctx.createLinearGradient(0, 0, 0, 225);
-        gradient.addColorStop(0, "rgba(215, 227, 244, 1)");
-        gradient.addColorStop(1, "rgba(215, 227, 244, 0)");
-        // Line chart
-        new Chart(document.getElementById("chartjs-dashboard-line"), {
-            type: "line",
-            data: {
-                labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-                datasets: [{
-                    label: "Sales ($)",
-                    fill: true,
-                    backgroundColor: gradient,
-                    borderColor: window.theme.primary,
-                    data: [
-                        2115,
-                        1562,
-                        1584,
-                        1892,
-                        1587,
-                        1923,
-                        2566,
-                        2448,
-                        2805,
-                        3438,
-                        2917,
-                        3327
-                    ]
-                }]
-            },
-            options: {
-                maintainAspectRatio: false,
-                legend: {
-                    display: false
-                },
-                tooltips: {
-                    intersect: false
-                },
-                hover: {
-                    intersect: true
-                },
-                plugins: {
-                    filler: {
-                        propagate: false
-                    }
-                },
-                scales: {
-                    xAxes: [{
-                        reverse: true,
-                        gridLines: {
-                            color: "rgba(0,0,0,0.0)"
-                        }
-                    }],
-                    yAxes: [{
-                        ticks: {
-                            stepSize: 1000
-                        },
-                        display: true,
-                        borderDash: [3, 3],
-                        gridLines: {
-                            color: "rgba(0,0,0,0.0)"
-                        }
-                    }]
-                }
-            }
-        });
-    });
-    document.addEventListener("DOMContentLoaded", function () {
-        // Pie chart
-        new Chart(document.getElementById("chartjs-dashboard-pie"), {
-            type: "pie",
-            data: {
-                labels: ["Chrome", "Firefox", "IE"],
-                datasets: [{
-                    data: [4306, 3801, 1689],
-                    backgroundColor: [
-                        window.theme.primary,
-                        window.theme.warning,
-                        window.theme.danger
-                    ],
-                    borderWidth: 5
-                }]
-            },
-            options: {
-                responsive: !window.MSInputMethodContext,
-                maintainAspectRatio: false,
-                legend: {
-                    display: false
-                },
-                cutoutPercentage: 75
-            }
-        });
-    });
-    document.addEventListener("DOMContentLoaded", function () {
-        // Bar chart
-        new Chart(document.getElementById("chartjs-dashboard-bar"), {
-            type: "bar",
-            data: {
-                labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-                datasets: [{
-                    label: "This year",
-                    backgroundColor: window.theme.primary,
-                    borderColor: window.theme.primary,
-                    hoverBackgroundColor: window.theme.primary,
-                    hoverBorderColor: window.theme.primary,
-                    data: [54, 67, 41, 55, 62, 45, 55, 73, 60, 76, 48, 79],
-                    barPercentage: .75,
-                    categoryPercentage: .5
-                }]
-            },
-            options: {
-                maintainAspectRatio: false,
-                legend: {
-                    display: false
-                },
-                scales: {
-                    yAxes: [{
-                        gridLines: {
-                            display: false
-                        },
-                        stacked: false,
-                        ticks: {
-                            stepSize: 20
-                        }
-                    }],
-                    xAxes: [{
-                        stacked: false,
-                        gridLines: {
-                            color: "transparent"
-                        }
-                    }]
-                }
-            }
-        });
-    });
-    return ( 
+
+    return (
         <div className="wrapper">
             <nav id="sidebar" className="sidebar js-sidebar">
                 <div className="sidebar-content js-simplebar">
@@ -325,9 +113,9 @@ const Index = () => {
 
             <div className="main">
                 <nav className="navbar navbar-expand navbar-light navbar-bg">
-                    <a className="sidebar-toggle js-sidebar-toggle">
+                    <p className="sidebar-toggle js-sidebar-toggle">
                         <i className="hamburger align-self-center"></i>
-                    </a>
+                    </p>
 
                     <div className="navbar-collapse collapse">
                         <ul className="navbar-nav navbar-align">
@@ -412,7 +200,7 @@ const Index = () => {
                                         <a href="#" className="list-group-item">
                                             <div className="row g-0 align-items-center">
                                                 <div className="col-2">
-                                                    <img src="img/avatars/avatar-5.jpg" className="avatar img-fluid rounded-circle" alt="Vanessa Tucker"/>
+                                                    <img src="img/avatars/avatar-5.jpg" className="avatar img-fluid rounded-circle" alt="Vanessa Tucker" />
                                                 </div>
                                                 <div className="col-10 ps-2">
                                                     <div className="text-dark">Vanessa Tucker</div>
@@ -424,7 +212,7 @@ const Index = () => {
                                         <a href="#" className="list-group-item">
                                             <div className="row g-0 align-items-center">
                                                 <div className="col-2">
-                                                    <img src="img/avatars/avatar-2.jpg" className="avatar img-fluid rounded-circle" alt="William Harris"/>
+                                                    <img src="img/avatars/avatar-2.jpg" className="avatar img-fluid rounded-circle" alt="William Harris" />
                                                 </div>
                                                 <div className="col-10 ps-2">
                                                     <div className="text-dark">William Harris</div>
@@ -436,7 +224,7 @@ const Index = () => {
                                         <a href="#" className="list-group-item">
                                             <div className="row g-0 align-items-center">
                                                 <div className="col-2">
-                                                    <img src="img/avatars/avatar-4.jpg" className="avatar img-fluid rounded-circle" alt="Christina Mason"/>
+                                                    <img src="img/avatars/avatar-4.jpg" className="avatar img-fluid rounded-circle" alt="Christina Mason" />
                                                 </div>
                                                 <div className="col-10 ps-2">
                                                     <div className="text-dark">Christina Mason</div>
@@ -448,7 +236,7 @@ const Index = () => {
                                         <a href="#" className="list-group-item">
                                             <div className="row g-0 align-items-center">
                                                 <div className="col-2">
-                                                    <img src="img/avatars/avatar-3.jpg" className="avatar img-fluid rounded-circle" alt="Sharon Lessman"/>
+                                                    <img src="img/avatars/avatar-3.jpg" className="avatar img-fluid rounded-circle" alt="Sharon Lessman" />
                                                 </div>
                                                 <div className="col-10 ps-2">
                                                     <div className="text-dark">Sharon Lessman</div>
@@ -639,7 +427,7 @@ const Index = () => {
                                         <h5 className="card-title mb-0">Real-Time</h5>
                                     </div>
                                     <div className="card-body px-4">
-                                        <div id="world_map" style="height:350px;"></div>
+                                        <div id="world_map" style={{ height: '350px' }}></div>
                                     </div>
                                 </div>
                             </div>
@@ -784,7 +572,7 @@ const Index = () => {
                     </div>
                 </footer>
             </div>
-        </div> 
+        </div>
     )
 }
 
