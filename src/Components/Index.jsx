40dce59fc7180cf19b34371b1c6b84210842d25f
bazil-1854 <bscs22072@itu.edu.pk>
2024-10-07 13:03:31 +0500

    Add "CoffeeStats" Component

--- src/Components/Index.jsx
+++ src/Components/Index.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import LiveCoffeeMaking from './LiveCoffeeMaking'
 import CoffeeServe from './CoffeeServe'
 import KindofCoffee from './KindofCoffee'
+import CoffeeStats from './CoffeeStats'
 
 const Index = () => {
     return (
         <div>
-            <header id="header-home" >
+            <header id="header" >
                 <div className="header-top">
                     <div className="container">
                         <div className="row justify-content-end">
@@ -69,70 +70,7 @@ const Index = () => {
             <LiveCoffeeMaking />
             <CoffeeServe />
             <KindofCoffee />
-            <section className="review-area section-gap" id="review">
-                <div className="container">
-                    <div className="row d-flex justify-content-center">
-                        <div className="menu-content pb-60 col-lg-10">
-                            <div className="title text-center">
-                                <h1 className="mb-10">What kind of Coffee we serve for you</h1>
-                                <p>Who are in extremely love with eco friendly system.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-lg-6 col-md-6 single-review">
-                            <img src="img/r1.png" alt="" />
-                            <div className="title d-flex flex-row">
-                                <h4>lorem ipusm</h4>
-                                <div className="star">
-                                    <span className="fa fa-star checked"></span>
-                                    <span className="fa fa-star checked"></span>
-                                    <span className="fa fa-star checked"></span>
-                                    <span className="fa fa-star"></span>
-                                    <span className="fa fa-star"></span>
-                                </div>
-                            </div>
-                            <p>
-                                Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
-                            </p>
-                        </div>
-                        <div className="col-lg-6 col-md-6 single-review">
-                            <img src="img/r2.png" alt="" />
-                            <div className="title d-flex flex-row">
-                                <h4>lorem ipusm</h4>
-                                <div className="star">
-                                    <span className="fa fa-star checked"></span>
-                                    <span className="fa fa-star checked"></span>
-                                    <span className="fa fa-star"></span>
-                                    <span className="fa fa-star"></span>
-                                    <span className="fa fa-star"></span>
-                                </div>
-                            </div>
-                            <p>
-                                Accessories Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker. Here you can find the best computer accessory for your laptop, monitor, printer, scanner, speaker.
-                            </p>
-                        </div>
-                    </div>
-                    <div className="row counter-row">
-                        <div className="col-lg-3 col-md-6 single-counter">
-                            <h1 className="counter">2536</h1>
-                            <p>Happy Client</p>
-                        </div>
-                        <div className="col-lg-3 col-md-6 single-counter">
-                            <h1 className="counter">7562</h1>
-                            <p>Total Projects</p>
-                        </div>
-                        <div className="col-lg-3 col-md-6 single-counter">
-                            <h1 className="counter">2013</h1>
-                            <p>Cups Coffee</p>
-                        </div>
-                        <div className="col-lg-3 col-md-6 single-counter">
-                            <h1 className="counter">10536</h1>
-                            <p>Total Submitted</p>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            <CoffeeStats />
 
             <section className="blog-area section-gap" id="blog">
                 <div className="container">
