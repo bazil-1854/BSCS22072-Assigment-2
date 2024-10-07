54f449c40a336e7c2fa8f60d1896f3da0f55cd53
bazil-1854 <bscs22072@itu.edu.pk>
2024-10-07 13:12:41 +0500

    Add "EcoFriendlySystem" Component

--- src/Components/Index.jsx
+++ src/Components/Index.jsx
@@ -4,11 +4,12 @@ import CoffeeServe from './CoffeeServe'
 import KindofCoffee from './KindofCoffee'
 import CoffeeStats from './CoffeeStats'
 import Navbar from './Navbar'
+import EcoFriendlySystem from './EcoFriendlySystem'
 
 const Index = () => {
     return (
         <div>
-            <Navbar/>
+            <Navbar />
             <section className="banner-area" id="home">
                 <div className="container">
                     <div className="row fullscreen d-flex align-items-center justify-content-start">
@@ -28,49 +29,7 @@ const Index = () => {
             <CoffeeServe />
             <KindofCoffee />
             <CoffeeStats />
-
-            <section className="blog-area section-gap" id="blog">
-                <div className="container">
-                    <div className="row d-flex justify-content-center">
-                        <div className="menu-content pb-60 col-lg-10">
-                            <div className="title text-center">
-                                <h1 className="mb-10">What kind of Coffee we serve for you</h1>
-                                <p>Who are in extremely love with eco friendly system.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-lg-6 col-md-6 single-blog">
-                            <img className="img-fluid" src="img/b1.jpg" alt="" />
-                            <ul className="post-tags">
-                                <li><a href="/">Travel</a></li>
-                                <li><a href="/">Life Style</a></li>
-                            </ul>
-                            <a href="/"><h4>Portable latest Fashion for young women</h4></a>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore  et dolore.
-                            </p>
-                            <p className="post-date">
-                                31st January, 2018
-                            </p>
-                        </div>
-                        <div className="col-lg-6 col-md-6 single-blog">
-                            <img className="img-fluid" src="img/b2.jpg" alt="" />
-                            <ul className="post-tags">
-                                <li><a href="/">Travel</a></li>
-                                <li><a href="/">Life Style</a></li>
-                            </ul>
-                            <a href="/"><h4>Portable latest Fashion for young women</h4></a>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore  et dolore.
-                            </p>
-                            <p className="post-date">
-                                31st January, 2018
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </section>
+            <EcoFriendlySystem />
 
             <footer className="footer-area section-gap">
                 <div className="container">
